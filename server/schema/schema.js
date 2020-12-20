c2c30e37a901538b2b2c8ffe5e9057b6b58b1d53
Trieu Truong <khactrieu.personal@gmail.com>
2020-12-20 22:57:15 +0700

    #9-The Resolve Function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
+
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  { name: 'The Final Empire', genre: 'Fantasy', is: '2'},
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3'};
+];
+
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -20,6 +30,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: {type: GraphQLString} },
       resolve(parent, args) {
         // code to get date from db / other source
+        return _.find(books, { id: agrs.id });
       }
     }
   }
